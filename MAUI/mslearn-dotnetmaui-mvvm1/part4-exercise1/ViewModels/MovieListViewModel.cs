c03be891b6a0b158ba72d6070c32e3497c15c733
Michal Jankowski <noisehunter82@gmail.com>
2025-06-13 14:59:13 +0100

    Add MAUI MVVM training project files.

---
+++ MAUI/mslearn-dotnetmaui-mvvm1/part4-exercise1/ViewModels/MovieListViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace MovieCatalog.ViewModels;
+
+public class MovieListViewModel: ObservableObject
+{
+    public ObservableCollection<MovieViewModel> Movies { get; set; }
+
+     private MovieViewModel? _selectedMovie;
+
+    public MovieViewModel? SelectedMovie
+    {
+        get => _selectedMovie;
+        set => SetProperty(ref _selectedMovie, value);
+    }
+    public ICommand DeleteMovieCommand { get; private set; }
+
+    public MovieListViewModel()
+    {
+        Movies = [];
+        DeleteMovieCommand = new Command<MovieViewModel>(DeleteMovie);
+    }
+
+    public async Task RefreshMovies()
+    {
+        //Movies = await Models.MoviesDatabase.GetMovies() as ObservableCollection<MovieViewModel> ?? [];
+         IEnumerable<Models.Movie> moviesData = await Models.MoviesDatabase.GetMovies();
+
+        foreach (Models.Movie movie in moviesData)
+            Movies.Add(new MovieViewModel(movie));
+    }
+
+    public void DeleteMovie(MovieViewModel movie) =>
+        Movies.Remove(movie);
+
+}
