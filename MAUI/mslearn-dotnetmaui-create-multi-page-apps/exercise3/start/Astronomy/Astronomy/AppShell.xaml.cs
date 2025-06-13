7d9a1dc04c4df7684cfd772b5fe55d34de3fe1ae
Michal Jankowski <noisehunter82@gmail.com>
2025-06-13 15:19:42 +0100

    Add MAUI navigation trainin. - Implement flyout navigation with .NET MAUI Shell. - Implement tab navigation with .NET MAUI Shell. - Navigate between pages within tabbed pages.

---
+++ MAUI/mslearn-dotnetmaui-create-multi-page-apps/exercise3/start/Astronomy/Astronomy/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Astronomy.Pages;
+
+namespace Astronomy;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+        // Add a route for the details page
+        Routing.RegisterRoute("astronomicalbodydetails", typeof(AstronomicalBodyPage));
+    }
+}
